25291cab10acb07edf85f9ebd9e158260748d1f9
Daaman Patel <daaman3110@gmail.com>
2025-07-19 08:27:17 +0530

    Added stock mail and removed personal mail

--- portscan.sh
+++ portscan.sh
@@ -14,7 +14,7 @@ nmap $ip_range | awk '/Nmap scan report/ {print "Target:" ,$5}
 echo "====================================================================================" >> $report_file
 
 # Step 4: Sending the mail
-recipient="daamanpatel4@gmail.com"
+recipient="yourmail@gmail.com"
 subject="Port Scan Report Audit - $(date '+%Y-%m-%d %H:%M:%S')"
 
 echo -e "Hi Team, \n\nPlease find attached the latest internal port scan audit.\n\nSummary:\n -Hosts scanned: 192.168.0.100-110\n -Open ports found: See attachment.\n\nRegards,\nDaaman Patel" | s-nail -s "$subject" -a "$report_file" "$recipient"
